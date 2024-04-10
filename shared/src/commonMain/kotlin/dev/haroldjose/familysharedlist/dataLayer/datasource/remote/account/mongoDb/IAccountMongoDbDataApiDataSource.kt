e7e966cc92756553ef002ffe6fe781a59c18669c
Harold Jose <hjose@blissapplications.com>
2024-04-10 21:21:17 +0100

    #18 - US05 - cleanner unused files

--- shared/src/commonMain/kotlin/dev/haroldjose/familysharedlist/dataLayer/datasource/remote/account/mongoDb/IAccountMongoDbDataApiDataSource.kt
+++
@@ -1,6 +0,0 @@
-package dev.haroldjose.familysharedlist.dataLayer.datasource.remote.account.mongoDb
-
-import dev.haroldjose.familysharedlist.dataLayer.datasource.remote.mongoDb.IMongoDbDataApiDataSource
-import dev.haroldjose.familysharedlist.dataLayer.dto.AccountDto
-
-interface IAccountMongoDbDataApiDataSource: IMongoDbDataApiDataSource<AccountDto>
